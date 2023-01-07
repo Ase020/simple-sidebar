3a96397e44b207f0fa4ece32e0370acf7013b764
Ase020 <68052150+Ase020@users.noreply.github.com>
2023-01-07 16:58:46 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const container = document.querySelector(".container");
+const linkItems = document.querySelector(".link-item");
+const darkMode = document.querySelector(".dark-mode");
+
+// Container Hover
+container.addEventListener("mouseenter", () => {
+  container.classList.add("active");
+});
+
+// Container Hover Leave
+container.addEventListener("mouseleave", () => {
+  container.classList.remove("active");
+});
+
+// Link-items Clicked
+for (let i = 0; i < linkItems.length; i++) {
+  if (!linkItems[i].classList.contains("dark-mode")) {
+    linkItems[i].addEventListener("click", (e) => {
+      linkItems.forEach((linkItem) => {
+        linkItem.classList.remove("active");
+      });
+      linkItems[i].classList.add("active");
+    });
+  }
+}
+
+// Dark Mode Functionality
+darkMode.addEventListener("click", () => {
+  if (document.body.classList.contains("dark-mode")) {
+    darkMode.querySelector("span").textContent = "dark mode";
+    darkMode.querySelector("ion-icon").setAttribute("name", "moon-outline");
+  } else {
+    darkMode.querySelector("span").textContent = "light mode";
+    darkMode.querySelector("ion-icon").setAttribute("name", "sunny-outline");
+  }
+  document.body.classList.toggle("dark-mode");
+});
